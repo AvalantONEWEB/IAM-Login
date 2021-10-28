7fab6e1c324121c5cefbeb99af6f0d26d8f16d7a
John Suri <34700043+johnsuri@users.noreply.github.com>
2021-10-28 11:58:22 +0700

    Update landingPage.js

--- landingPage.js
+++ landingPage.js
@@ -18,37 +18,3 @@ function printstates($page, event) {
     console.log("print state function ends");
     $page.update();
     }
-    
-//Call Logout API - Logout from the Password Credential Grant Flow
-function userlogoutIAM2PC($page, event) {
-    //alert ("Start Logout");  
-    var token1 = sessionStorage.getItem("iam2_token_login_jhn") || localStorage.getItem("ct_token");
-    alert("User Session logged out. Thankyou for using ONEWEB IAM login Demo - using Password Credential Flow ;) ");
-    var username = sessionStorage.getItem("usernameA") || localStorage.getItem("usernameA");
-    //var form = new FormData();
-    var settings = {
-      "url": "https://community.oneweb.tech/IAM2RESTService/oauth/logout",
-      "method": "POST",
-      "timeout": 0,
-      "headers": {
-        "Authorization": "Bearer " + token1,
-      },
-    };
-  
-    $.ajax(settings).done(function (response) {
-      //remove all credentails from the local/session storage
-      sessionStorage.removeItem("iam2_token_login_jhn");
-      sessionStorage.removeItem("usernameA");
-      sessionStorage.removeItem("firstname");
-      sessionStorage.removeItem("lastname");
-      sessionStorage.removeItem("department");
-      sessionStorage.removeItem("mobile");
-      sessionStorage.removeItem("email");
-      sessionStorage.removeItem("avatar");
-      sessionStorage.removeItem("userid");
-      //Redirect user back to the Sign IN Page
-      $page.pageGo("loginwithiam2");
-      $page.update();
-      // alert ("End Logout")
-    });
-  }
